289c594b4a177878f7ba7af9463191fcf7fb7142
braib <bhavishraib@gmail.com>
2025-02-10 21:58:00 +0530

    added world file on tw_robot_sim

--- two_robot_sim/setup.py
+++ two_robot_sim/setup.py
@@ -14,6 +14,8 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
+
 
     ],
     install_requires=['setuptools'],
